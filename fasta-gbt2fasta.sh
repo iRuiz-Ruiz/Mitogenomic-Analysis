63464415143741486142ee0295e27c6b0eb6f296
M. Isabel Ruiz Ruiz <95575812+iRuiz-Ruiz@users.noreply.github.com>
2023-03-22 13:21:12 -0500

    Add files via upload

---
+++ fasta-gbt2fasta.sh
@@ -0,0 +1,13 @@
+for i in *.fasta-gb
+do
+#delete enter in all the file
+tr -d "\r\n" < $i > dummy
+tr -d " " < dummy > dummy2
+#includes an "enter" per specie
+sed -i -E "s/>/\n>/g" dummy2
+#delete first empty row
+awk 'NR>1' dummy2 > dummy
+#add enter to ID, only accept eight characters 
+sed -e "s/>.\{8\}/&\n/g" < dummy > ${i-gb}.fasta
+rm dummy dummy2
+done
---
+++ mafft.sh
@@ -0,0 +1,4 @@
+for i in *.fasta
+do
+    mafft --quiet $i > ${i%.fasta}.aligned.fasta
+done
